a146c8407e2a73d45b1ea0dcbc5677f70787db50
Pavan Hassamal <phass001@gmail.com>
2018-09-05 20:41:08 -0700

    alphabeticShift

---
+++ alphabeticShift/alphabeticShift.js
@@ -0,0 +1,18 @@
+function alphabeticShift(inputString){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+
+    let inputShifted = inputString.split('');
+
+    for(let i = 0; i < inputShifted.length; i++){
+        let index = 0;
+
+        if(inputShifted[i] !== 'z'){
+            index = alphabet.indexOf(inputShifted[i]) + 1;
+        }
+        inputShifted[i] = alphabet[index];
+    }
+    return inputShifted.join('');
+}
+
+console.log(alphabeticShift('crazy'));
