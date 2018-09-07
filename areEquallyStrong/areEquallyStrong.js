3dae5b52433b4bc7ac69a9c8378f20c8f0bf637b
Pavan Hassamal <phass001@gmail.com>
2018-09-07 08:17:26 -0700

    areEquallyStrong

---
+++ areEquallyStrong/areEquallyStrong.js
@@ -0,0 +1,13 @@
+function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight){
+    const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+    const yourStrongest = yourLeft >= yourRight? yourLeft : yourRight;
+    const friendsWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+    const friendsStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+
+    return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
+
+}
+
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
